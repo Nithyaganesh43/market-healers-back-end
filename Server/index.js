8c290c1ad6cd4f2546b04b2a9d65f855e2fc60fb
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-05 22:54:15 +0530

    stock data

---
+++ Server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const ping_pong = require('./src/ping-pong');
+const api = require('./api');
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+app.use(ping_pong);
+app.use(api);
+
+app.use((req, res) => res.send('Hello from server 1'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
